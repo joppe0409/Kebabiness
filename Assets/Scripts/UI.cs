2df0e2e82340af4c1c7e33c2ed432bb2d2c1fa4f
TSGMeher <tsgmeher@gmail.com>
2021-10-28 10:31:32 +0200

    n

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
-    public Image healthBar;
+    Image healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        healthBar = GetComponent<Image>();
     }
 
     // Update is called once per frame
@@ -19,7 +19,12 @@ public class UI : MonoBehaviour
         
     }
 
-  public float getHealth() // Tar värdet av hur mycket healthbar är fylld - Meher
+    public void SetHealthBarColor(Color healthColor)
+    {
+        healthBar.color = healthColor;
+    }
+
+    public float getHealth() // Tar värdet av hur mycket healthbar är fylld - Meher
     {
         return healthBar.fillAmount;
     }   
@@ -27,7 +32,19 @@ public class UI : MonoBehaviour
     public void UpdateHealth(float value) // Sätter mängden healthbar är fylld till en variabel - Meher
     {
         healthBar.fillAmount = value;
-        
+        if (healthBar.fillAmount < 0.3f)
+        {
+            SetHealthBarColor(Color.red);
+        }
+        else if (healthBar.fillAmount < 0.7f)
+        {
+            SetHealthBarColor(Color.yellow);
+        }
+        else
+        {
+            SetHealthBarColor(Color.green);
+        }
+
     }
  
 }
